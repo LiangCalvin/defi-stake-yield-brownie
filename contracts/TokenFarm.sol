17d3e9c76ddd11a48e02ca650c6d94e96416458d
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-03 00:01:57 +0700

    update staketoken, add issuetokens

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -2,22 +2,61 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TokenFarm is Ownable {
     // stake token
     // unstake token
     // issue token
     // add allow token
     // getEthValue
-    address[] public allowedTokens;
 
+    // mapping token address -> staker address -> amount
+    mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+    address[] public stakers;
+    address[] public allowedTokens;
+    IERC20 public dappToken;
+    // for every 1 ETH, we give 1 DappToken
+    // 50 ETH and 50 DAI staked, give 1 DAPP per 1 DAI
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress);
+    }
+    function issueTokens() public onlyOwner {
+        // Issue tokens to all stakers
+        for (
+            uint256 stakersIndex = 0;
+            stakersIndex < stakers.length;
+            stakersIndex++
+        ) {
+            address recipient = stakers[stakersIndex];
+            uint256 userTotalValue = getUserTotalValue(recipient);
+            // send them a token reward
+            // based on their total value locked
+
+            dappToken.transfer(recipient, amount);
+        }
+    }
     function stakeTokens(uint256 _amount, address _token) public {
         // what token can they stake
         // how much can they stake
         require(_amount > 0, "Amount must be more than 0");
-        // require(_token is allow);
+        require(tokenIsAllowed(_token), "Token is currently no allowed");
+        // transferFrom
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStaked(msg.sender, _token);
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
     }
 
+    function updateUniqueTokensStaked(address _user, address _token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokenStaked[_user] = uniqueTokenStaked[_user] + 1;
+        }
+    }
     function addAllowedTokens(address _token) public onlyOwner {
         allowedTokens.push(_token);
     }
