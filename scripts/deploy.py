f810e9359f7c816174281b2df5e1441fcd61c2f7
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-11 22:52:09 +0700

    add add_allowed_token

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,8 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, config, network
+from web3 import Web3
+
+KEPT_BALANCE = Web3.to_wei(100, "ether")
 
 
 def deploy_token_farm_and_dapp_token():
@@ -10,6 +13,26 @@ def deploy_token_farm_and_dapp_token():
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
+    tx = dapp_token.transfer(
+        token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+    # 3 tokens allowed, dapp_token, weth_token, fau_token/dai
+    # get_contract will deploy mock weth token
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    dic_of_allowed_tokens = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
+    add_allowed_tokens(
+        token_farm,
+    )
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    pass
 
 
 def main():
