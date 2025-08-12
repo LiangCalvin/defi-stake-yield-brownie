25d66dfc749c241fb89fa7e7998ed9f9592a3edf
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-12 11:26:42 +0700

    add test_issue_token

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -3,6 +3,7 @@ from scripts.helpful_scripts import (
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
     get_account,
     get_contract,
+    INITIAL_PRICE_FEED_VALUE,
 )
 import pytest
 from scripts.deploy import deploy_token_farm_and_dapp_token
@@ -52,5 +53,22 @@ def test_stake_tokens(amount_staked):
     return token_farm, dapp_token
 
 
-def test_issue_token():
-    pass
+def test_issue_token(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for unit local testing")
+
+    account = get_account()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    starting_balance = dapp_token.balanceOf(account.address)
+
+    # Act
+    token_farm.issueTokens({"from": account})
+
+    # Assert we are staking 1 dapp == in price to 1 eth
+    # so we should get 2,000 dapp in rewards
+    # since the price of eth is 2000 usd
+    assert (
+        dapp_token.balanceOf(account.address)
+        == starting_balance + INITIAL_PRICE_FEED_VALUE
+    )
