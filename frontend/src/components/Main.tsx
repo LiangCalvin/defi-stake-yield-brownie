f625db2b0bc0a448f86a3532bef91e940f7e6b69
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-14 23:27:04 +0700

    add update_front_end

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export const Main = () => {
+    // show token value
+    // get address of different tokens
+    // get balance of user wallet
+    // send yaml to src folder
+    // send build folder 
+
+    // return (
+
+    // )
+}
