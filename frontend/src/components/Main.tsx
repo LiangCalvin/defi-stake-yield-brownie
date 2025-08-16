5ed9c6058558f17ec0060b15c7e8161dfd39a687
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-16 17:31:56 +0700

    add supportedTOkens

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -3,6 +3,9 @@ import helperConfig from "../helper-config.json";
 import networkMapping from "../chain-info/deployments/map.json";
 import { constants } from "ethers";
 import brownieConfig from "../brownie-config.json";
+import dapp from "../dapp.png"
+import eth from "../eth.png"
+import dai from "../dai.png"
 
 interface ContractAddresses {
     DappToken: string[];
@@ -16,6 +19,12 @@ interface NetworkMapping {
 
 const networkMappingTyped: NetworkMapping =
     networkMapping as unknown as NetworkMapping;
+
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
 
 export const Main = () => {
     const { account, chainId } = useEthers();
@@ -41,12 +50,34 @@ export const Main = () => {
         chainId && networkName
             ? (brownieConfig["networks"] as any)[networkName]?.["weth_token"]
             : constants.AddressZero;
+    console.log(wethTokenAddress);
     const fauTokenAddress =
         chainId && networkName
             ? (brownieConfig["networks"] as any)[networkName]?.["fau_token"]
             : constants.AddressZero;
+    console.log(fauTokenAddress);
+
+    const supportedTokens: Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image: eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: dai,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
+
     return (
         <div>
+            {/* <YourWallet supportedTokens={supportedTokens} /> */}
             <p>
                 Connected to {networkName} ({chainId})
             </p>
