872adbcbc014ad3446fc193584b2ef4c7105edad
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-16 15:41:53 +0700

    add config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,16 @@ import { Header } from './components/Header';
 import Container from '@mui/material/Container';
 import { Main } from './components/Main';
 
+const config = {
+  supportedChains: [ChainId.Sepolia],
+  readOnlyChainId: ChainId.Sepolia,
+  readOnlyUrls: {
+    [ChainId.Sepolia]: 'https://sepolia.infura.io/v3/761d2363bdb147f4babc971dd1a4c921'
+  }
+};
 function App() {
   return (
-    <DAppProvider config={{
-      supportedChains: [ChainId.Sepolia]
-    }} >
+    <DAppProvider config={config} >
       <Header />
       <Container maxWidth='md'>
         Hi
