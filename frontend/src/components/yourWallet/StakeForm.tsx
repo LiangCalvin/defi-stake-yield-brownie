f3aee441ef6ffb534a71c1f57bf926d63cac1ac0
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-08-24 10:17:59 +0700

    add approve

--- frontend/src/components/yourWallet/StakeForm.tsx
+++ frontend/src/components/yourWallet/StakeForm.tsx
@@ -5,6 +5,8 @@ import Button from "@mui/material/Button"
 import SendIcon from '@mui/icons-material/Send';
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
+import { useStakeTokens } from "../../hooks/useStakeTokens";
+import { utils } from "ethers";
 
 export interface StakeFormProps {
     token: Token
@@ -18,7 +20,7 @@ export const StakeForm = ({ token }: StakeFormProps) => {
     // const [loading, setLoading] = React.useState(true);
     const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
 
-    // function handleClick() {
+    // function handleStakeSubmit() {
     //     setLoading(true);
     // }
 
@@ -29,12 +31,18 @@ export const StakeForm = ({ token }: StakeFormProps) => {
         console.log("amount", amount)
     }
 
+    const { approve, approveErc20State } = useStakeTokens(tokenAddress)
+    const handleStakeSubmit = () => {
+        const amountAsWei = utils.parseEther(amount.toString())
+        return approve(amountAsWei.toString())
+    }
+
     return (
         <div>
             <TextField onChange={handleInputChange} id="standard-basic" label="please fill amount" variant="standard" />
             <Button
                 size="small"
-                // onClick={handleClick}
+                onClick={handleStakeSubmit}
                 loadingPosition="end"
                 variant="contained"
             >
